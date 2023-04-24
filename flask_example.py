f918e9382b1f1941e28293b14571ccaa86f90f69
Vadim Turushev <orchanin90@gmail.com>
2023-04-24 21:52:00 +0500

    add dynamic route '/courses/<id>'

--- flask_example.py
+++ flask_example.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Welcome to Flask it is very easy hello'
+
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users', 302
+
+
+@app.route('/courses/<id>')
+def courses(id):
+    return f'Course id: {id}'
